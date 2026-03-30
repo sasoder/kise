ced4fd645385182b935ad4a4245b2388b66dc076
Samuel Söderberg <samuelsoderberg99@gmail.com>
2026-03-30 16:30:17 +0200

    prores

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,7 @@
 import { Config } from "@remotion/cli/config";
 
+Config.setCodec("prores");
+Config.setProResProfile("4444");
 Config.setVideoImageFormat("png");
+Config.setPixelFormat("yuva444p10le");
 Config.setOverwriteOutput(true);
